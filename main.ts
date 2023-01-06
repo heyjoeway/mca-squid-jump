6238b33d5649b7e29c1ff474948d39cda8e2ae20
Joseph Judge <4339897+heyjoeway@users.noreply.github.com>
2023-01-05 20:22:33 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,29 @@
-let tilemapCurrent = tilemap`level`;
-tiles.setCurrentTilemap(tilemapCurrent);
-
+function getBGPos () {
+    return 0 - scene.cameraProperty(CameraProperty.Y) + game.runtime() / 100
+}
+function sfxWin () {
+    timer.background(function () {
+        music.playMelody("G5", 500)
+        music.playMelody("-", 500)
+        music.playMelody("G5", 550)
+        music.playMelody("C6", 150)
+    })
+}
+let tilemapCurrent = tilemap`level`
+tiles.setCurrentTilemap(tilemapCurrent)
 class ObjSquiddy {
     defaultImg = assets.animation`squid`[0];
     sprite: Sprite = null;
     timeJumpStart = 0;
+    stopped = false;
 
     constructor() {
         this.sprite = sprites.create(this.defaultImg, SpriteKind.Player);
         this.sprite.setStayInScreen(false);
 
-        this.sprite.ay = 225
-        scene.cameraFollowSprite(this.sprite)
+        this.sprite.ay = 225       
         this.sprite.y = tilemapCurrent.height * 16 - 8
+        this.sprite.x = 6 * 16;
 
         // Squid jump anim start
         controller.A.onEvent(
@@ -28,6 +39,8 @@ class ObjSquiddy {
     }
 
     startJump() {
+        if (this.stopped) return;
+
         animation.runImageAnimation(
             this.sprite,
             assets.animation`squid`,
@@ -38,6 +51,8 @@ class ObjSquiddy {
     }
 
     releaseJump() {
+        if (this.stopped) return;
+
         animation.stopAnimation(
             animation.AnimationTypes.ImageAnimation,
             this.sprite
@@ -55,9 +70,64 @@ class ObjSquiddy {
             jumpChargeTimeMax
         ) / jumpChargeTimeMax) * jumpPowerMax
         this.sprite.vy = Math.min(this.sprite.vy, jumpPower)
+
+        music.playSoundEffect(
+            music.createSoundEffect(WaveShape.Square, 1902, 253, 88, 7, 250, SoundExpressionEffect.Tremolo, InterpolationCurve.Logarithmic),
+            SoundExpressionPlayMode.UntilDone
+        );
+    }
+
+    loopCamera() {
+        scene.centerCameraAt(6 * 16, this.sprite.y);
+    }
+
+    loopBounds() {
+        let leftBound = 8;
+        let rightBound = leftBound * 2 + screen.width;
+        if (this.sprite.x < leftBound) this.sprite.x = rightBound;
+        else if (this.sprite.x > rightBound) this.sprite.x = leftBound;
+    }
+
+    stop() {
+        this.stopped = true;
+        this.sprite.vx = 0;
+        this.sprite.vy = 0;
+        this.sprite.ax = 0;
+        this.sprite.ay = 0;
+    }
+
+    win() {
+        this.stop();
+        sfxWin();
+        
+        let bgSprite = sprites.create(assets.image`winBg`, SpriteKind.Text);
+        bgSprite.x = scene.cameraLeft() + (screen.width / 2);
+        bgSprite.y = scene.cameraTop() + (screen.height / 2);
+
+        let textItems = [
+            { x: 0, y: -12, text: "GOAL!" },
+            { x: 0, y: 0, text: "STAGE BONUS 300" },
+            { x: 0, y: 12, text: "TIME BONUS 100" }
+        ];
+
+        textItems.forEach(item => {
+            let textSprite = textsprite.create(item.text);
+            textSprite.x = item.x + scene.cameraLeft() + (screen.width / 2);
+            textSprite.y = item.y + scene.cameraTop() + (screen.height / 2);
+            textSprite.setMaxFontHeight(6);
+        });
+    }
+
+    loopTiles() {
+        let tileLocation = this.sprite.tilemapLocation();
+
+        if (tiles.tileAtLocationEquals(tileLocation, assets.tile`zapfish`))
+            this.win();
     }
 
     loop() {
+        if (this.stopped) return;
+
         if (this.sprite.vy < 0) {
             this.sprite.setFlag(SpriteFlag.GhostThroughWalls, true);
         } else {
@@ -74,21 +144,15 @@ class ObjSquiddy {
             this.sprite.ax = squiddyAcc;
         else
             this.sprite.ax = 0;
+
+        this.loopBounds();
+        this.loopCamera();
+        this.loopTiles();
     }
 }
-
 scene.setBackgroundImage(assets.image`bg`)
-
-function getBGPos() {
-    return (
-        -scene.cameraProperty(CameraProperty.Y) +
-        game.runtime() / 100
-    );
-}
-
-forever(() => {
-    // Adjust BG pos
-    scroller.setBackgroundScrollOffset(0, getBGPos());
-});
-
 let squiddy = new ObjSquiddy();
+forever(function () {
+    // Adjust BG pos
+    scroller.setBackgroundScrollOffset(0, getBGPos())
+})
